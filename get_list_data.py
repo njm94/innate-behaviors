8d686a3b90f3f551c492901167837a478eb14386
Pankaj Gupta <guptapg@tcd.ie>
2024-08-12 15:23:05 -0700

    Added new experiments to get_list_data and modified helper to be more flexible with video input

--- get_list_data.py
+++ get_list_data.py
@@ -54,52 +54,79 @@ expt1_data_list.append(['ECR2_thy1', [
   
 expt2_data_list = [] # 2p grooming
 
-expt2_data_list.append(['ETL3_thy1', [
-    '20231109110512', # spontaneous
-    '20231111193330', # spontaneous
-    '20231113134311', # evoked
-    '20231115163212', # evoked
-    ]])
+# expt2_data_list.append(['ETL3_thy1', [
+#     '20231109110512', # spontaneous
+#     '20231111193330', # spontaneous
+#     '20231113134311', # evoked
+#     '20231115163212', # evoked
+#     ]])
    
-expt2_data_list.append(['ETR3_thy1', [
-    '20231109092652', # spontaneous
-    '20231111153211', # spontaneous
-    '20231113155903', # evoked
-    '20231115174148', # evoked
-    ]])
+# expt2_data_list.append(['ETR3_thy1', [
+#     '20231109092652', # spontaneous
+#     '20231111153211', # spontaneous
+#     '20231113155903', # evoked
+#     '20231115174148', # evoked
+#     ]])
     
-expt2_data_list.append(['ETR2_thy1', [
-    '20231109115418', # spontaneous
-    '20231109115952', # spontaneous
-    '20231109120816', # spontaneous
-    '20231111165721', # spontaneous
-    '20231113143925', # evoked
-    '20231115153531', # evoked
-    ]])
-    
-expt2_start_end_idx = []
-expt2_start_end_idx.append(['ETL3_thy1', [
-    (70,  64457),
-    (0,  109736),
-    (62, 109799),
-    (66, 109805),
+# expt2_data_list.append(['ETR2_thy1', [
+#     '20231109115418', # spontaneous
+#     '20231109115952', # spontaneous
+#     '20231109120816', # spontaneous
+#     '20231111165721', # spontaneous
+#     '20231113143925', # evoked
+#     '20231115153531', # evoked
+#     ]])
+
+expt2_data_list.append(['ECL3_thy1', [
+    '20240722', # spontaneous
+    '20240724', # spontaneous
+    '20240726', # spontaneous
+    '20240729', # evoked
+    '20240731', # evoked
+    '20240802', # evoked
     ]])
 
-expt2_start_end_idx.append(['ETR3_thy1', [
-    (71, 109887),
-    (62, 109855),
-    (66, 109755),
-    (61, 109797),
+expt2_data_list.append(['IDR3_tTA6s', [
+    '20240722', # spontaneous
+    '20240724', # spontaneous
+    '20240726', # spontaneous
+    '20240729', # evoked
+    '20240731', # evoked
+    '20240802', # evoked
     ]])
 
-expt2_start_end_idx.append(['ETR2_thy1', [
-    (70, 18831),
-    (64, 29573),
-    (61, 23064),
-    (0, 109798),
-    (65, 109844),
-    (66, 109795)
+expt2_data_list.append(['RR3_tTA8s', [
+    '20240722', # spontaneous
+    '20240724', # spontaneous
+    '20240726', # spontaneous
+    '20240729', # evoked
+    '20240731', # evoked
+    '20240802', # evoked
     ]])
+    
+# expt2_start_end_idx = []
+# expt2_start_end_idx.append(['ETL3_thy1', [
+#     (70,  64457),
+#     (0,  109736),
+#     (62, 109799),
+#     (66, 109805),
+#     ]])
+
+# expt2_start_end_idx.append(['ETR3_thy1', [
+#     (71, 109887),
+#     (62, 109855),
+#     (66, 109755),
+#     (61, 109797),
+#     ]])
+
+# expt2_start_end_idx.append(['ETR2_thy1', [
+#     (70, 18831),
+#     (64, 29573),
+#     (61, 23064),
+#     (0, 109798),
+#     (65, 109844),
+#     (66, 109795)
+#     ]])
 
 
 expt3_data_list = [] # 1p drinking
