0678599bac43d89d71bace1446562d42d4289b09
Pankaj Gupta <guptapg@tcd.ie>
2024-11-08 10:50:21 -0800

    Added linkage clustering on UMAP embeddings for behavior classification

--- create_behavior_features.m
+++ create_behavior_features.m
@@ -26,7 +26,11 @@
 
 clc, clear
 fileID = fopen('expt1_datalist.txt','r');
-addpath('C:\Users\user\Documents\Nick\grooming\utils')
+if isunix
+    addpath('/home/user/Documents/grooming/utils')
+else
+    addpath('C:\Users\user\Documents\Nick\grooming\utils')
+end
 
 formatSpec = '%s';
 data_list = textscan(fileID, formatSpec);
@@ -64,16 +68,12 @@ mp_idx = 26:28;
 %% 
 
 % initialize feature and label matrices
-fMat = []; 
-lMat = [];
-
+bFilepath = {};
+behavior_index = [];
 
 behavior_duration = [];
 behavior_label = [];
-l_paw_start_x = [];
-l_paw_start_y = [];
-r_paw_start_x = [];
-r_paw_start_y = [];
+
 l_paw_yrange = [];
 l_paw_xrange = [];
 r_paw_yrange = [];
@@ -88,6 +88,27 @@ fll_max_speed = [];
 flr_max_speed = [];
 fll_avg_speed = [];
 flr_avg_speed = [];
+
+
+% Position features
+fll_xpos_start = []; fll_ypos_start = []; % left paw (x,y) at start
+flr_xpos_start = []; flr_ypos_start = []; % right paw (x,y) at start
+fll_xpos_mid = []; fll_ypos_mid = []; % left paw (x,y) at midpoint
+flr_xpos_mid = []; flr_ypos_mid = []; % right paw (x,y) at midpoint
+fll_xpos_end = []; fll_ypos_end = []; % left paw (x,y) at end
+flr_xpos_end = []; flr_ypos_end = []; % right paw (x,y) at end
+fll_xpos_atmax_vL = []; fll_ypos_atmax_vL = []; % left paw (x,y) at time of max velocity of left paw
+flr_xpos_atmax_vR = []; flr_ypos_atmax_vR = []; % right paw (x,y) at time of max velocity of right paw
+fll_xpos_atmax_vR = []; fll_ypos_atmax_vR = []; % left paw (x,y) at time of max velocity of right paw
+flr_xpos_atmax_vL = []; flr_ypos_atmax_vL = []; % right paw (x,y) at time of max velocity of left paw
+
+
+fll_vx_atmax_vL = []; fll_vy_atmax_vL = [];
+flr_vx_atmax_vR = []; flr_vy_atmax_vR = [];
+fll_vx_atmax_vR = []; fll_vy_atmax_vR = [];
+flr_vx_atmax_vL = []; flr_vy_atmax_vL = [];
+
+corr_x = []; corr_y = [];
 
 %%
 
@@ -106,7 +127,7 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
         [expt_root_dir, ~, ~] = fileparts(mouse_root_dir);
     
         new_mouse = ~strcmp(mouse_id, current_mouse);
-    catch
+     catch
         new_mouse = true;
     end
     if new_mouse 
@@ -125,6 +146,7 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
     try
         dlc_pos_file = [data_dir, filesep, get_file_with_str(data_dir, '1030000.csv')];
         dlc_speed_file = [data_dir, filesep, get_file_with_str(data_dir, 'speed.csv')];
+        dlc_vel_file = [data_dir, filesep, get_file_with_str(data_dir, 'vel.csv')];
         boris_file = [data_dir, filesep, get_file_with_str(data_dir, 'events.tsv')];
     catch
         disp('Missing one or more of the required files. Skipping...')
@@ -157,28 +179,33 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
     nose_x = median(dlc_pos(:,1));
     nose_y = median(dlc_pos(:,2));
     
-    flr_x = dlc_pos(:,4);
-    flr_y = dlc_pos(:,5);
-    
-    fll_x = dlc_pos(:,7);
-    fll_y = dlc_pos(:,8);
+    % get all position data relative to nose to account for variability
+    % in camera positioning across trials
+    flr_x = nose_x - dlc_pos(:,4);
+    flr_y = nose_y - dlc_pos(:,5);    
+    fll_x = nose_x - dlc_pos(:,7);
+    fll_y = nose_y - dlc_pos(:,8);
 
     dlc_speed = readmatrix(dlc_speed_file);
     fll_speed = dlc_speed(:,1);
     flr_speed = dlc_speed(:,2);
 
-    %%
-    figure
-    for i = 1:size(stroke_events,2)
-        tmp = logical(table2array(stroke_events(:,i)));
-        subplot(1,7,i), hold on
-        plot(flr_x(tmp), -flr_y(tmp))
-        plot(fll_x(tmp), -fll_y(tmp))
-        axis([200 450 -250 0])
-        title(stroke_events.Properties.VariableNames{i})
-    end
+    dlc_vel = readmatrix(dlc_vel_file);
+    flr_vx = dlc_vel(:,4);
+    flr_vy = dlc_vel(:,5);
+    fll_vx = dlc_vel(:,7);
+    fll_vy = dlc_vel(:,8);
 
-%     figure, plot(flr_x(table2array(stroke_events(:,1)), -flr_y(table2array(stroke_events(:,1)))))
+    %%
+    % figure
+    % for i = 1:size(stroke_events,2)
+    %     tmp = logical(table2array(stroke_events(:,i)));
+    %     subplot(1,7,i), hold on
+    %     plot(flr_x(tmp), -flr_y(tmp))
+    %     plot(fll_x(tmp), -fll_y(tmp))
+    %     axis([200 450 -250 0])
+    %     title(stroke_events.Properties.VariableNames{i})
+    % end
 
 
 %%
@@ -200,10 +227,15 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
     for i = 1:length(stroke_types)
         idx = strcmpi(stroke_types{i}, boris.Behavior);
         behavior_frames = boris.ImageIndex(idx);
-
+        
+        % test = arr2idx(table2array(stroke_events(:,i)));
+        % start_idx = test(:,1);
+        % stop_idx = test(:,2);
         
         start_idx = behavior_frames(1:2:end);
         stop_idx = behavior_frames(2:2:end);
+
+        behavior_index = cat(1, behavior_index, [start_idx, stop_idx]);
 
         if length(start_idx) > 1
             % flag events that occur immediately one after the other
@@ -221,12 +253,19 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
         imm_events = cat(1, imm_events, flag_events);
 
 
-        l_paw_start_x = cat(1, l_paw_start_x, nose_x-fll_x(start_idx));
-        l_paw_start_y = cat(1, l_paw_start_y, nose_y-fll_y(start_idx));
-        r_paw_start_x = cat(1, r_paw_start_x, nose_x-flr_x(start_idx));
-        r_paw_start_y = cat(1, r_paw_start_y, nose_y-flr_y(start_idx));
+        fll_xpos_start = cat(1, fll_xpos_start, fll_x(start_idx));
+        fll_ypos_start = cat(1, fll_ypos_start, fll_y(start_idx));
+        flr_xpos_start = cat(1, flr_xpos_start, flr_x(start_idx));
+        flr_ypos_start = cat(1, flr_ypos_start, flr_y(start_idx));
+
+        fll_xpos_end = cat(1, fll_xpos_end, fll_x(stop_idx));
+        fll_ypos_end = cat(1, fll_ypos_end, fll_y(stop_idx));
+        flr_xpos_end = cat(1, flr_xpos_end, flr_x(stop_idx));
+        flr_ypos_end = cat(1, flr_ypos_end, flr_y(stop_idx));
 
         for j = 1:length(start_idx)
+            bFilepath = cat(1, bFilepath, boris_file);
+
             l_paw_yrange = cat(1, l_paw_yrange, range(fll_y(start_idx(j):stop_idx(j))));
             l_paw_xrange = cat(1, l_paw_xrange, range(fll_x(start_idx(j):stop_idx(j))));
             r_paw_yrange = cat(1, r_paw_yrange, range(flr_y(start_idx(j):stop_idx(j))));
@@ -240,7 +279,7 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
             % interspersed with lick
             trial_length = length(lick_events);
             try
-                lick_flag = any(lick_events(start_idx(j):stop_idx(j)+30));
+                lick_flag = any(lick_events(start_idx(j)-3:stop_idx(j)+3));
             catch
                 lick_flag = any(lick_events(start_idx(j):end));
             end
@@ -258,12 +297,49 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
             flr_avg_speed = cat(1, flr_avg_speed, mean(flr_speed(start_idx(j):stop_idx(j))));
 
             % max speed
-            fll_max_speed = cat(1, fll_max_speed, mean(fll_speed(start_idx(j):stop_idx(j))));
-            flr_max_speed = cat(1, flr_max_speed, mean(flr_speed(start_idx(j):stop_idx(j))));
+            [maxfllspeed, argmaxfllspeed] = max(fll_speed(start_idx(j):stop_idx(j)));
+            [maxflrspeed, argmaxflrspeed] = max(flr_speed(start_idx(j):stop_idx(j)));
+            fll_max_speed = cat(1, fll_max_speed, maxfllspeed);
+            flr_max_speed = cat(1, flr_max_speed, maxflrspeed);
 
-            % ratio of distance traveled
+            % position at max speed of same paw
+            fll_xpos_atmax_vL = cat(1, fll_xpos_atmax_vL, fll_x(start_idx(j)+argmaxfllspeed));
+            fll_ypos_atmax_vL = cat(1, fll_ypos_atmax_vL, fll_y(start_idx(j)+argmaxfllspeed));
+            flr_xpos_atmax_vR = cat(1, flr_xpos_atmax_vR, flr_x(start_idx(j)+argmaxflrspeed));
+            flr_ypos_atmax_vR = cat(1, flr_ypos_atmax_vR, flr_y(start_idx(j)+argmaxflrspeed));
+
+            % position at max speed of opposite paw
+            fll_xpos_atmax_vR = cat(1, fll_xpos_atmax_vR, fll_x(start_idx(j)+argmaxflrspeed));
+            fll_ypos_atmax_vR = cat(1, fll_ypos_atmax_vR, fll_y(start_idx(j)+argmaxflrspeed));
+            flr_xpos_atmax_vL = cat(1, flr_xpos_atmax_vL, flr_x(start_idx(j)+argmaxfllspeed));
+            flr_ypos_atmax_vL = cat(1, flr_ypos_atmax_vL, flr_y(start_idx(j)+argmaxfllspeed));
+
+            % midpoint position
+            fll_xpos_mid = cat(1, fll_xpos_mid, fll_x(round(mean([start_idx(j), stop_idx(j)]))));
+            fll_ypos_mid = cat(1, fll_ypos_mid, fll_y(round(mean([start_idx(j), stop_idx(j)]))));
+            flr_xpos_mid = cat(1, flr_xpos_mid, flr_x(round(mean([start_idx(j), stop_idx(j)]))));
+            flr_ypos_mid = cat(1, flr_ypos_mid, flr_y(round(mean([start_idx(j), stop_idx(j)]))));
+
+            % velocity of paw at same paw's max speed (to capture direction)
+            fll_vx_atmax_vL = cat(1, fll_vx_atmax_vL, fll_vx(start_idx(j)+argmaxfllspeed));
+            fll_vy_atmax_vL = cat(1, fll_vy_atmax_vL, fll_vy(start_idx(j)+argmaxfllspeed));
+            flr_vx_atmax_vR = cat(1, flr_vx_atmax_vR, flr_vx(start_idx(j)+argmaxflrspeed));
+            flr_vy_atmax_vR = cat(1, flr_vy_atmax_vR, flr_vy(start_idx(j)+argmaxflrspeed));
+
+            % velocity of paw at opposite paw's max speed (to capture direction)
+            fll_vx_atmax_vR = cat(1, fll_vx_atmax_vR, fll_vx(start_idx(j)+argmaxflrspeed));
+            fll_vy_atmax_vR = cat(1, fll_vy_atmax_vR, fll_vy(start_idx(j)+argmaxflrspeed));
+            flr_vx_atmax_vL = cat(1, flr_vx_atmax_vL, flr_vx(start_idx(j)+argmaxfllspeed));
+            flr_vy_atmax_vL = cat(1, flr_vy_atmax_vL, flr_vy(start_idx(j)+argmaxfllspeed));
+
+
+            % distance traveled
             fll_distance = cat(1, fll_distance, sum(fll_speed(start_idx(j):stop_idx(j))));
             flr_distance = cat(1, flr_distance, sum(flr_speed(start_idx(j):stop_idx(j))));
+
+            % correlation of paw trajectories
+            corr_x = cat(1, corr_x, corr(fll_x, flr_x));
+            corr_y = cat(1, corr_y, corr(fll_y, flr_y));
 
         end
 
@@ -273,247 +349,46 @@ for j = 1:length(data_list{1})%mp_idx %23:length(data_list{1})+1
 
 end
 
+% TO DO: compute instantaneous rate for behaviors
+% direction at max speed
+
+
+
+%%
+
+% use features that emphasize difference between right and left to
+% better separate left/right actions
 
 y_diff = l_paw_max_y - r_paw_max_y;
+d_diff = fll_distance - flr_distance;
+v_diff = fll_avg_speed - flr_avg_speed;
+ystart_diff = fll_ypos_start - flr_ypos_start;
+xstart_diff = fll_xpos_start - flr_xpos_start;
+yrange_diff = l_paw_yrange - r_paw_yrange;
+xrange_diff = l_paw_xrange - r_paw_xrange;
+xmid_diff = fll_xpos_mid - flr_xpos_mid;
+ymid_diff = fll_ypos_mid - flr_ypos_mid;
+xend_diff = fll_xpos_end - flr_xpos_end;
+yend_diff = fll_ypos_end - flr_ypos_end;
+fll_range_ratio = l_paw_xrange./l_paw_yrange;
+flr_range_ratio = r_paw_xrange./r_paw_yrange;
 
-%%
-
-test = [behavior_duration, l_paw_xrange, l_paw_yrange, l_paw_start_y, ...
-    l_paw_start_x, r_paw_xrange, r_paw_yrange, r_paw_start_y, ...
-    r_paw_start_x, y_diff, lick_interspersed, fll_distance, flr_distance, ...
-    imm_events, fll_avg_speed, flr_avg_speed, fll_max_speed, flr_max_speed];
-
-[coeff, score, latent] = pca(test-mean(test));
-[C, ia, ic] = unique(behavior_label);
-
-%%
-figure, hold on
-% h=biplot(coeff(:,1:3),'scores',score(:,1:3));
-
-for i = 1:size(behavior_label,1)
-    switch behavior_label{i}
-        case 'Elliptical'
-            c = 'y';
-            mkr = 'o';
-        case 'Elliptical Asymmetric'
-%             continue
-            c = 'k';
-            mkr = 'o';
-        case 'Large Bilateral'
-%             continue
-            c = 'g';
-            mkr = 'o';
-        case 'Left'
-%             continue
-            c = 'c';
-            mkr = 'o';
-        case 'Right'
-%             continue
-            c = 'm';
-            mkr = 'o';
-        case 'Left Asymmetric'
-%             continue
-            c = 'c';
-            mkr = 'd';
-        case 'Right Asymmetric'
-%             continue
-            c = 'm';
-            mkr = 'd';
-    end
-    scatter3(score(i,1), score(i,2), score(i,3), c, mkr, 'filled', 'MarkerFaceAlpha', 0.25);
-end
-
-%%
-
-[a,b] = kmeans(test, 3);
-
-cols = ['r', 'g', 'b', 'm', 'c', 'y', 'k'];
-
-
-figure, hold on
-% h=biplot(coeff(:,1:3),'scores',score(:,1:3));
-
-for i = 1:size(behavior_label,1)
-    scatter3(score(i,1), score(i,2), score(i,3), cols(a(i)), 'filled', 'MarkerFaceAlpha', 0.25);
-end
-
-
-
-%% 
-
-% Train a random forest classifier to predict the cluster labels
-random_forest_model = fitcensemble(test, behavior_label, 'Method', 'Bag', 'NumLearningCycles', 100);
-
-%%
-
-% Perform cross-validation to get accuracy
-cv_model = crossval(random_forest_model);
-classification_error = kfoldLoss(cv_model);
-
-% Convert classification error to accuracy
-classification_accuracy = 1 - classification_error;
-fprintf('Classification Accuracy: %.2f%%\n', classification_accuracy * 100);
-
-%%
-
-% Compute feature importance
-feature_importance = oobPermutedPredictorImportance(random_forest_model);
-
-% Plot feature importance
-figure
-bar(feature_importance);
-xlabel('Feature');
-ylabel('Importance Score');
-title('Feature Importance for Cluster Separation');
-
-
-%%
-
-% Predict cluster labels using the trained random forest
-predicted_labels = predict(random_forest_model, test);
-
-% Confusion matrix
-figure
-confusionchart(behavior_label, predicted_labels);
-
-%%
-
-% Use cross-validation to evaluate the model
-cv_model = crossval(random_forest_model);  % Cross-validate the model
-predicted_labels = kfoldPredict(cv_model); % Predict labels from cross-validation
+featureMat = zscore([behavior_duration, l_paw_xrange, l_paw_yrange, fll_ypos_start, ...
+    fll_xpos_start, r_paw_xrange, r_paw_yrange, flr_ypos_start, ...
+    flr_xpos_start, y_diff,  fll_distance, flr_distance, ...
+    fll_avg_speed, flr_avg_speed, fll_max_speed, flr_max_speed, ...
+    d_diff, v_diff, ystart_diff, xstart_diff, xrange_diff, yrange_diff, ...
+    fll_xpos_atmax_vL, fll_ypos_atmax_vL, flr_xpos_atmax_vR, flr_ypos_atmax_vR, ...
+    fll_xpos_mid, fll_ypos_mid, flr_xpos_mid, flr_ypos_mid, xmid_diff, ymid_diff, ...
+    fll_xpos_end, fll_ypos_end, flr_xpos_end, flr_ypos_end, xend_diff, yend_diff, ...
+    fll_xpos_atmax_vR, fll_ypos_atmax_vR, flr_xpos_atmax_vL, flr_ypos_atmax_vL, ... 
+    fll_range_ratio, flr_range_ratio]);
 
 
 
 %%
-clc
-unique_classes = unique(behavior_label);
-n_classes = length(unique_classes);
-weights = zeros(n_classes, 1);
-
-for i = 1:n_classes
-    n_i = sum(strcmp(behavior_label, unique_classes{i})); % Count of samples in class i
-    weights(i) = numel(behavior_label) / n_i; % Inverse frequency weight
-end
-
-% Create a weight map
-class_weights = containers.Map(unique_classes, weights);
-
-
-% Create a stratified cross-validation partition
-cv = cvpartition(behavior_label, 'KFold', 10, 'Stratify', true);
-
-% Initialize arrays to store performance metrics
-accuracy = zeros(cv.NumTestSets, 1);
-f1_scores = zeros(cv.NumTestSets, 1);
-
-% Loop through each fold
-for i = 1:cv.NumTestSets
-    % Get training and test indices
-    trainIdx = cv.training(i);
-    testIdx = cv.test(i);
-    
-    % Train the model on the training set
-    rf_model = fitcensemble(test(trainIdx, :), behavior_label(trainIdx), 'Method', 'Bag', 'NumLearningCycles', 100, 'Weights', class_weights);
-    
-    % Make predictions on the test set
-    Y_pred = predict(rf_model, test(testIdx, :));
-    
-    % Calculate accuracy
-    accuracy(i) = sum(Y_pred == behavior_label(testIdx)) / numel(behavior_label(testIdx));
-    
-    % Calculate F1-score for cluster 2
-    % Assuming cluster 2 corresponds to label 2
-    tp = sum((Y_pred == 2) & (behavior_label(testIdx) == 2)); % True Positives
-    fp = sum((Y_pred == 2) & (behavior_label(testIdx) ~= 2)); % False Positives
-    fn = sum((Y_pred ~= 2) & (behavior_label(testIdx) == 2)); % False Negatives
-    
-    precision = tp / (tp + fp); % Precision for cluster 2
-    recall = tp / (tp + fn); % Recall for cluster 2
-    
-    if tp + fp == 0 || tp + fn == 0
-        f1_scores(i) = 0; % Handle division by zero
-    else
-        f1_scores(i) = 2 * (precision * recall) / (precision + recall); % F1-score for cluster 2
-    end
-end
-
-% Average accuracy and F1-score across folds
-avg_accuracy = mean(accuracy);
-avg_f1_score = mean(f1_scores);
-fprintf('Average Accuracy: %.2f%%\n', avg_accuracy * 100);
-fprintf('Average F1-Score for Cluster 2: %.2f\n', avg_f1_score);
-
-
-%%
-
-figure
-
-bl2 = behavior_label;
-for i = 1:length(behavior_label)
-    switch bl2{i}
-        case 'Left'
-            continue
-        case 'Right'
-            continue
-        otherwise
-            bl2{i} = 'Biltaeral';
-    end
-end
-
-%%
-figure
-[s,h] = silhouette(test, bl2);
-
-%%
-
-z=linkage(test);
-size(z)
-
-
-%%
-
-
-figure, hold on
-% h=biplot(coeff(:,1:3),'scores',score(:,1:3));
-
-for i = 1:size(behavior_label,1)
-    switch behavior_label{i}
-        case 'Elliptical'
-            c = 'k';
-            mkr = 'o';
-        case 'Elliptical Asymmetric'
-            c = 'c';
-            mkr = 'd';
-        otherwise
-            continue
-    end
-    scatter3(score(i,1), score(i,2), score(i,3), c, mkr)
-end
-
-%%
-counter = 1;
-for i = 1:length(h)
-    if strcmpi(h(i).LineStyle, 'none')
-        if counter <= ia(2)
-            h(i).MarkerEdgeColor = 'k';
-        elseif counter <= ia(3)
-            h(i).MarkerEdgeColor = 'b';
-        elseif counter <= ia(4)
-            h(i).MarkerEdgeColor = 'm';
-        elseif counter <= ia(5)
-            h(i).MarkerEdgeColor = 'c';
-        elseif counter <= ia(6)
-            h(i).MarkerEdgeColor = 'g';
-        else
-            h(i). MarkerEdgeColor = 'r';
-        end
-        counter = counter + 1;
-
-    end
-end
-
-
-%%
+disp('Saving')
+save('/media/user/teamshare/nick/behavior/grooming/umap_test.mat', 'featureMat', 'bFilepath', 'behavior_label', 'behavior_index')
 
 
 
@@ -525,6 +400,5 @@ end
 
 
 
-%%
-events = read_boris('Y:\nick\behavior\grooming\1p\ECR2_thy1\20231116161015\ECR2_thy1_20231116161015_events.tsv');
-figure, plot(events.("Right Asymmetric"))
+
+
