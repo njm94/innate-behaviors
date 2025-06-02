378b6793a50ca327c01f6b5401d0b7ea5b3718f4
Nick Michelson <nick.michelson94@gmail.com>
2025-06-02 11:18:50 -0700

    Cleaning

--- deprecated/get_neuron_location_in_dorsal_space.m
+++
@@ -1,8 +0,0 @@
-function [x,y] = get_neuron_location_in_dorsal_space(x0,y0,roi_tform, linear_tform, wfi_tform)   
-    
-    [x1, y1] = transformPointsForward(roi_tform.tform, x0, y0);        
-    [x2, y2] = transformPointsForward(linear_tform.tform, x1, y1);        
-    [x, y] = transformPointsForward(wfi_tform.tform, x2, y2);
-
-
-end
